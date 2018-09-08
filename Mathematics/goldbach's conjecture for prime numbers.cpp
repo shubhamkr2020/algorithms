d7324c6451afddca044666a03ab2fe5596096a5c
shubhamkr2020 <shubhamkr2020@gmail.com>
2018-09-08 17:06:03 +0530

    added goldbach's conjecture

---
+++ Mathematics/goldbach's conjecture for prime numbers.cpp
@@ -0,0 +1,3 @@
+// Conjecture - Every even number greaer than equal to 4 can be expressed as a sum of two prime numbers.
+
+// https://www.geeksforgeeks.org/program-for-goldbachs-conjecture-two-primes-with-given-sum/
