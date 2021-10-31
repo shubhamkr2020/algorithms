c0cb63c61fd6023433d957cf7b883e0871759059
Shubham Kumar <shukum@microsoft.com>
2021-10-31 17:22:27 +0530

    changes

--- Mathematics/XOR Problems.cpp
+++ Mathematics/XOR Problems.cpp
@@ -1,6 +1,7 @@
 /*
 
-https://www.geeksforgeeks.org/count-number-subarrays-given-xor/				---- prefix xor concept
-https://www.geeksforgeeks.org/subarray-xor-less-k/							---- trie
+https://www.geeksforgeeks.org/count-number-subarrays-given-xor/								---- prefix xor concept
+https://www.geeksforgeeks.org/subarray-xor-less-k/											---- trie
+https://www.geeksforgeeks.org/maximum-possible-xor-every-element-array-another-array/   	---- trie
 
 */
