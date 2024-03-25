7253cf6c011576cde584857a749e02975291cc02
Shubham Kumar <shubhamkr2020@gmail.com>
2024-03-25 13:09:39 +0530

    Update 1. binary search implementations.cpp

--- Binary Search/1. binary search implementations.cpp
+++ Binary Search/1. binary search implementations.cpp
@@ -1,2 +1,26 @@
 // https://codeforces.com/blog/entry/9901
 // https://codeforces.com/blog/entry/9901?#comment-153756
+
+
+int BinarySearch(vector<int>& v, int target) {
+  int n = v.size();
+  int l=-1,r=n;
+  
+  while(r-l>1) {
+    int m = (l+r)/2;
+    if(v[m] < target) l=m;
+    else r=m;
+  }
+  return r;
+}
+
+/*
+Here, finall values for l and r:
+l will give you the first number which is less than the target.
+r will give you the first number which is grater than equal to target.
+i.e. 
+v[l] < target
+v[r] >= target
+
+So, to find first number which is greater than target use the opposite.
+*/
