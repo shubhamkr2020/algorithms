3c879f667dc49a99c092594daf83fe0f8f5c6818
Shubham Kumar <shubhamkr2020@gmail.com>
2024-03-27 15:26:39 +0530

    Update LeetCode.cpp

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,6 +6,7 @@ Questions with concepts (must do quesitons)
 3. https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii - stocks buy and sell
 4. https://leetcode.com/problems/strobogrammatic-number-ii/?envType=list&envId=pfyhj5d6 - recursion
 5. https://leetcode.com/problems/next-permutation/description/ - very imp.
+      https://leetcode.com/problems/next-permutation/solutions/3169948/best-c-2-solution-two-pointers-optimized-solution/
 
 
 */
