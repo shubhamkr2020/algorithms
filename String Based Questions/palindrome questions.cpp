c70c2a031557c6141311b4a83fe6a88138e81440
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-05 16:41:39 +0530

    Create palindrome questions.cpp

---
+++ String Based Questions/palindrome questions.cpp
@@ -0,0 +1,9 @@
+/*
+
+For any question, try to expand from center, first for odd length and then for evne length.
+
+ 1. longest palindromic subsequence
+ 2. longest palindromic substring - manacher's algo.
+
+
+*/
