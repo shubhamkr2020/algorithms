152777af5766f1586e0befc5cc43deb1e679c71f
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-05 08:38:25 +0530

    Create leetcode.cpp

---
+++ rand()/leetcode.cpp
@@ -0,0 +1,7 @@
+/*
+
+1. https://leetcode.com/problems/random-pick-with-weight/ - probability and rand() ...IMP...
+2. https://leetcode.com/problems/random-pick-index/ - rand() 
+3. https://leetcode.com/problems/shuffle-an-array/description/ - rand()
+
+*/
