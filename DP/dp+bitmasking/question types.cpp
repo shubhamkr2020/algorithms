1dd6de8df5f840b4734fd45e721ee5514167b2b8
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-07 21:45:21 +0530

    Update question types.cpp

--- DP/dp+bitmasking/question types.cpp
+++ DP/dp+bitmasking/question types.cpp
@@ -2,7 +2,7 @@
 
 1. Find max subset sum less than k
 2. Find minimum cost for n jobs and n person with given cost matrix
-3. Find k equal sum subsets - https://leetcode.com/problems/partition-to-k-equal-sum-subsets/description/
+3. Find k equal sum subsets - https://leetcode.com/problems/partition-to-k-equal-sum-subsets/description/ - backtracking with bitmask and explore all subsets.
 4. Travelling salesman problem
 5. number of ways to fill n*m grd with 1*2 brick horizontally or vertically
   dp(i,mask) = till ith column is filled and (i+1)th column is represented by the mask.
