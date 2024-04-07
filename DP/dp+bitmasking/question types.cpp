4ea3118dabd68a811463760d2e90b9eb3e27a765
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-07 21:33:02 +0530

    Update question types.cpp

--- DP/dp+bitmasking/question types.cpp
+++ DP/dp+bitmasking/question types.cpp
@@ -4,6 +4,8 @@
 2. Find minimum cost for n jobs and n person with given cost matrix
 3. Find k equal sum subsets - https://leetcode.com/problems/partition-to-k-equal-sum-subsets/description/
 4. Travelling salesman problem
+5. number of ways to fill n*m grd with 1*2 brick horizontally or vertically
+  dp(i,mask) = till ith column is filled and (i+1)th column is represented by the mask.
 
 
 Process to solve these problems:
