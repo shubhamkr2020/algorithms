367cbc36a72d037a2ad32529bcf8b8e1950e7719
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-07 15:48:14 +0530

    Create krushkal's algorithm for mst.cpp

---
+++ Graphs/krushkal's algorithm for mst.cpp
@@ -0,0 +1,9 @@
+/*
+
+https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
+ - use union find on the sorted edges and avoid cycles and create the minimum spanning tree (tree with minimum weighted edges)
+
+............IMPPPPPPPP.............
+Concept - remember that we can traverse the edges as well if needed.
+
+*/
