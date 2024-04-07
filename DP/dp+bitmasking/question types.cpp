e550f495d1972c08c608d7817eb75bf4a1782748
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-07 20:57:02 +0530

    Create question types.cpp

---
+++ DP/dp+bitmasking/question types.cpp
@@ -0,0 +1,15 @@
+/*
+
+1. Find max subset sum less than k
+2. Find minimum cost for n jobs and n person with given cost matrix
+3. Find k equal sum subsets
+4. Travelling salesman problem
+
+
+Process to solve these problems:
+
+1. Do a bruteforce approach first - NP hard problem.
+2. find the subset mask and use dp to memoize the answers.
+
+
+*/
