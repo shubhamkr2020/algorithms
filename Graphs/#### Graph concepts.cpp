a05e5556920323de935ce17a0a45677c85f70813
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-07 16:31:08 +0530

    Create #### Graph concepts.cpp

---
+++ Graphs/#### Graph concepts.cpp
@@ -0,0 +1,25 @@
+/*
+
+Directed graphs:
+
+1. Try to use topological sorting if possible.
+2. 
+
+
+Undirected Graphs:
+
+1. Ignore the parent and you can assume it to be a directed graph while traversal for many algorithms.
+2. 
+
+
+For Both:
+
+1. You can also traverse the edges only if needed for a few algorithms like floyd-warshall and krushkal's
+2. Use coloring of the node while traversal if needed, colors can be white (not visited yet), grey (in the queue/stack) or black (completely explored)
+3. DSU can also be used sometimes. like detecting the cycles
+4. 
+
+
+
+
+*/
