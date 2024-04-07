709932dc3d319a81b596c8e2252d919445fe9448
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-07 22:07:53 +0530

    Update #### Graph concepts.cpp

--- Graphs/#### Graph concepts.cpp
+++ Graphs/#### Graph concepts.cpp
@@ -15,11 +15,23 @@ Undirected Graphs:
 For Both:
 
 1. You can also traverse the edges only if needed for a few algorithms like floyd-warshall and krushkal's
-2. Use coloring of the node while traversal if needed, colors can be white (not visited yet), grey (in the queue/stack) or black (completely explored)
-3. DSU can also be used sometimes. like detecting the cycles
+2. Use coloring of the node while traversal if needed, colors can be white (not visited yet), grey (in process) or black (completely explored)
+3. DSU can also be used sometimes. like detecting the cycles in krushkal's
 4. 
 
 
+Unweighted graphs:
 
+1. bfs, dfs
+3. topological sort
+4. lca
+
+Weighted Graphs:
+
+1. Dijkstra's
+2. bellman ford
+3. floyyd warshall
+4. 0-1 bfs
+5. krushkal's mst using union find with no cycle
 
 */
