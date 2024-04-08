6aa710d7518b2b300c657f044ac54a44ba8eecde
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-08 09:52:49 +0530

    Update palindrome questions.cpp

--- String Based Questions/palindrome questions.cpp
+++ String Based Questions/palindrome questions.cpp
@@ -1,6 +1,6 @@
 /*
 
-For any question, try to expand from center, first for odd length and then for evne length.
+For any question, try to expand from center, first for odd length and then for even length.
 
  1. longest palindromic subsequence
  2. longest palindromic substring - manacher's algo.
