fcc85900a168ac17ad6d28c84c79f54be808b163
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-08 18:17:20 +0530

    Create backtracking concept.cpp

---
+++ backtracking/backtracking concept.cpp
@@ -0,0 +1,14 @@
+/*
+
+bool res = false;
+for(i=0 to all possible cases)
+  mark something and 
+  res = res || (try for something using the above marked)
+  unmark that thing
+
+return res;
+
+
+
+
+*/
