035a339af5a3e83d9f14e986d166df5a7f46ecc7
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-10 18:41:49 +0530

    Update backtracking concept.cpp

--- backtracking/backtracking concept.cpp
+++ backtracking/backtracking concept.cpp
@@ -1,10 +1,15 @@
 /*
 
 bool res = false;
-for(i=0 to all possible cases)
-  mark something and 
+
+for(i=0 to all possible cases) {
+  mark something/multiple things and
+
+  // do something logical like:
   res = res || (try for something using the above marked)
-  unmark that thing
+  
+  unmark all those things
+}
 
 return res;
 
