62ab72188883e62a2fa69532bf0cf9af9d5f2db3
Shubham Kumar <shubhamkr2020@gmail.com>
2024-04-26 18:07:10 +0530

    Update question types.cpp

--- backtracking/question types.cpp
+++ backtracking/question types.cpp
@@ -3,5 +3,6 @@
 1. N queen problem - https://www.geeksforgeeks.org/printing-solutions-n-queen-problem/
 2. Find all the sets with sum = k - https://leetcode.com/problems/partition-to-k-equal-sum-subsets/description/
 3. find all unique permutations with duplicate numbers: https://leetcode.com/problems/permutations-ii/
+4. https://leetcode.com/problems/optimal-account-balancing/ - 
 
 */
