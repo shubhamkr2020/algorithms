f719c3f52cf4f14b5f8878b1471682cd71163883
shubhamaddictrose <72778506+shubhamaddictrose@users.noreply.github.com>
2024-06-02 00:24:41 +0530

    Create multi_dimension_dp.cpp

---
+++ DP/multi_dimension_dp.cpp
@@ -0,0 +1,5 @@
+/* if n actor are accessing same resources and any one can get those resources then use n-Dim array to store current resource row.
+
+https://leetcode.com/problems/cherry-pickup-ii/solutions/4708659/intuitive-full-code-explained-java-c-c-javascript/
+*/
+https://leetcode.com/problems/cherry-pickup-ii/description/
