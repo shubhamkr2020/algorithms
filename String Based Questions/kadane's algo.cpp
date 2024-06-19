465ad5e2027ee69732b34c39bf3c5b7789eebf1a
Shubham Kumar <shubhamkr2020@gmail.com>
2024-06-19 16:55:23 +0530

    Update kadane's algo.cpp

--- String Based Questions/kadane's algo.cpp
+++ String Based Questions/kadane's algo.cpp
@@ -1,1 +1,3 @@
+
+// concept - choose characters and iterate through the strings 26 times if possible to find the solution by itertion the string for each character.
 1. https://leetcode.com/problems/substring-with-largest-variance/solutions/3738611/beats-100-fast-solution-unordered-set-fully-optimized/
